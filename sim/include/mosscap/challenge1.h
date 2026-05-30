2e6ae3c427edd3d2fc2223506a7c49a179e50ff1
West <westly.bouchard@gmail.com>
2026-05-29 18:31:30 -0600

    Update include-able setup files to utilize defaults and add challenge1.h

---
+++ sim/include/mosscap/challenge1.h
@@ -0,0 +1,73 @@
+//
+// Created by west on 5/29/26.
+//
+
+#ifndef MOSSCAP_CHALLENGE1_H
+#define MOSSCAP_CHALLENGE1_H
+
+#include <imgui.h>
+
+#include "defaults.h"
+#include "sim/TankSim.h"
+
+void drawPath();
+
+inline std::unique_ptr<SimulatorBase> simInit() {
+    // Set rendering scale
+    Renderer::setScale(3.0);
+    Renderer::getInstance().registerDrawFunction(-2, drawPath);
+
+    auto config = defaultRobot;
+    config.showEncoderTelemetry = false;
+    config.showTOFTelemetry = false;
+
+    config.kineticFriction = 0.05;
+
+    config.noiseMagnitude = 0.1;
+
+    // Create robot
+    auto robot = std::make_unique<TankSim>(defaultRobot);
+    robot->setPose(0.6, 0.75, 0);
+
+    TOFConfig tofConfig{0, 0, 0};
+    tofConfig.boundingBox = BoundingBox(3, 3);
+
+    robot->registerTOF(tofConfig);
+
+    robot->registerLeftMotor(defaultMotor, 10);
+    robot->registerRightMotor(defaultMotor, 20);
+
+    robot->registerLeftEncoder(1500, 15);
+    robot->registerRightEncoder(1500, 25);
+
+    ArduinoRuntime::getInstance().createButton("Button", 1);
+
+    return robot;
+}
+
+static constexpr std::array<std::array<double, 2>, 8> pathPoints = {{
+    {0.6, 0.75},
+    {2.1, 0.75},
+    {2.1, 1.45},
+    {2.4, 1.45},
+    {2.4, 2.25},
+    {1.2, 2.25},
+    {1.2, 1.75},
+    {0.6, 1.75}
+}};
+
+inline void drawPath() {
+    std::array<ImVec2, pathPoints.size() + 1> vertices;
+    for (auto&& [p, v] : std::views::zip(pathPoints, vertices)) {
+        v.x = p.at(0) * pxPerMeter + 400;
+        v.y = p.at(1) * pxPerMeter + 400;
+    }
+
+    // This is kind of hacky but whatever
+    vertices.at(8) = vertices.at(0);
+    vertices.at(8).y += 3.1;
+
+    ImGui::GetWindowDrawList()->AddPolyline(vertices.data(), vertices.size(), 0xFF00FF00, 0, 7.0);
+}
+
+#endif //MOSSCAP_CHALLENGE1_H
--- sim/include/mosscap/defaultTank.h
+++ sim/include/mosscap/defaultTank.h
@@ -7,15 +7,14 @@
 
 #include "sim/TankSim.h"
 
-std::unique_ptr<SimulatorBase> simInit() {
+#include "defaults.h"
+
+inline std::unique_ptr<SimulatorBase> simInit() {
     // Set rendering scale
     Renderer::setScale(2.0);
 
-    // Configuration of the robot
-    WMRConfig config{2.0 /*kg*/, 0.20 /*m*/, 0.15 /*m*/, 0.04 /*m*/};
-
     // Create robot
-    auto robot = std::make_unique<TankSim>(config);
+    auto robot = std::make_unique<TankSim>(defaultRobot);
     robot->setPose(1, 1, 0);
 
     TOFConfig tofConfig{0, 0, 0};
@@ -23,16 +22,11 @@ std::unique_ptr<SimulatorBase> simInit() {
 
     robot->registerTOF(tofConfig);
 
-    // Configuration of motors
-    // In theory you could have a different config for each motor if you wanted
-    // But in this case each motor is exactly the same
-    const MotorConfig mConfig{3.1, 0.1245, 2.3, 6.0};
+    robot->registerLeftMotor(defaultMotor, 10);
+    robot->registerRightMotor(defaultMotor, 20);
 
-    robot->registerLeftMotor(mConfig, 10);
-    robot->registerRightMotor(mConfig, 11);
-
-    robot->registerLeftEncoder(1500, 20);
-    robot->registerRightEncoder(1500, 21);
+    robot->registerLeftEncoder(1500, 15);
+    robot->registerRightEncoder(1500, 25);
 
     ArduinoRuntime::getInstance().createButton("Button", 1);
 
