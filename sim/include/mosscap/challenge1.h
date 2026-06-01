65e8974234908234462e8d2aab645bb385d12525
West <westly.bouchard@gmail.com>
2026-05-31 18:12:40 -0600

    Add second, more complex path, to challenge 1

--- sim/include/mosscap/challenge1.h
+++ sim/include/mosscap/challenge1.h
@@ -56,6 +56,17 @@ static constexpr std::array<std::array<double, 2>, 8> pathPoints = {{
     {0.6, 1.75}
 }};
 
+static constexpr std::array<std::array<double, 2>, 8> pathPoints2 = {{
+    {0.6, 0.75},
+    {2.1, 0.6},
+    {1.9, 1.45},
+    {2.4, 1.6},
+    {2.55, 2.25},
+    {1.2, 2.4},
+    {1.0, 1.75},
+    {0.6, 1.55}
+}};
+
 inline void drawPath() {
     std::array<ImVec2, pathPoints.size() + 1> vertices;
     for (auto&& [p, v] : std::views::zip(pathPoints, vertices)) {
