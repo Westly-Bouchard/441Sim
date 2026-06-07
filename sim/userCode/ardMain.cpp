b53a4f4d8273e6deefc45c320403452251dc5f53
West <westly.bouchard@gmail.com>
2026-06-07 17:26:50 -0600

    chore: User code

--- sim/userCode/ardMain.cpp
+++ sim/userCode/ardMain.cpp
@@ -1,202 +1,67 @@
-#include "Sim.h"
+#include "mosscap/challenge2.h"
 
-#include "AStarPlanner.h"
-#include "Drivetrain.h"
-#include "Mapping.h"
+#include <Encoder.h>
+#include <Motor.h>
 
-// Button Pins
-const int SENSE_BUTTON = 1;
-const int PLAN_BUTTON = 2;
-const int ACT_BUTTON = 3;
-const int CONTINUE_BUTTON = 4;
+Motor left(11, 10, 12);
+Motor right(21, 20, 22);
 
-// Left Motor Pins
-const int L_M_ENA = 11;
-const int L_M_IN_1 = 10;
-const int L_M_IN_2 = 12;
+Encoder leftEncoder(15, 16);
+Encoder rightEncoder(25, 26);
 
-// Right Motor Pins
-const int R_M_ENA = 21;
-const int R_M_IN_1 = 20;
-const int R_M_IN_2 = 22;
+TelemetryPosition pose;
 
-// Left Encoder Pins
-const int L_E_A = 15;
-const int L_E_B = 16;
+const double metersPerCount = 1.673e-4;
+const double trackWidth = 0.20;
 
-// Right Encoder Pins
-const int R_E_A = 25;
-const int R_E_B = 26;
+void updatePosition() {
+    double leftDistance = leftEncoder.readAndReset() * metersPerCount;
+    double rightDistance = rightEncoder.readAndReset() * metersPerCount;
 
-// Mapping constants
-const double MAP_SIZE = 3.0;
-const double MAP_RESOLUTION = 0.1;
+    double d = (leftDistance + rightDistance) / 2.0;
+    double dTheta = (rightDistance - leftDistance) / trackWidth;
 
-// Start and end position of the robot
-const double START_X = 0.5;
-const double START_Y = 0.5;
+    // Convert from degrees to radians
+    double thetaOld = pose.getTheta() * PI / 180.0;
 
-const double GOAL_X = 1.8;
-const double GOAL_Y = 2.6;
+    double xNew = pose.getX() + d * cos(thetaOld + dTheta / 2.0);
+    double yNew = pose.getY() + d * sin(thetaOld + dTheta / 2.0);
+    double thetaNew = thetaOld + dTheta;
 
-const double MAX_MOVE_DIST = 0.5;
+    pose.setX(xNew);
+    pose.setY(yNew);
 
-double getPhysicalDist(const Location a, const Location b) {
-    const double aX = a.x * MAP_RESOLUTION + 0.5 * MAP_RESOLUTION;
-    const double aY = a.y * MAP_RESOLUTION + 0.5 * MAP_RESOLUTION;
-
-    const double bX = b.x * MAP_RESOLUTION + 0.5 * MAP_RESOLUTION;
-    const double bY = b.y * MAP_RESOLUTION + 0.5 * MAP_RESOLUTION;
-
-    return sqrt(pow(aX - bX, 2) + pow(aY - bY, 2));
-}
-
-// Physical robot hardware
-Motor mL(L_M_ENA, L_M_IN_1, L_M_IN_2);
-Motor mR(R_M_ENA, R_M_IN_1, R_M_IN_2);
-
-Encoder eL(L_E_A, L_E_B);
-Encoder eR(R_E_A, R_E_B);
-
-VL53L0X tof;
-
-// Current position of the robot from odometry calculations
-TelemetryPose pose;
-
-// Current global obstacle map
-TelemetryMap map(MAP_SIZE, MAP_RESOLUTION);
-
-// Planner
-AStarPlanner planner(map);
-
-std::vector<Location> plan() {
-    // Determine map locations of current location and goal
-    Location current{
-        static_cast<int>(std::floor(pose.getX() / MAP_RESOLUTION)),
-        static_cast<int>(std::floor(pose.getY() / MAP_RESOLUTION))
-    };
-
-    Location goal{
-        static_cast<int>(std::floor(GOAL_X / MAP_RESOLUTION)),
-        static_cast<int>(std::floor(GOAL_Y / MAP_RESOLUTION))
-    };
-
-    planner.reset(current, goal);
-    auto path = planner.solve();
-
-    return path;
-}
-
-void act(const std::vector<Location>& path) {
-    // Traverse the path until a direction change is detected or until MAX_MOVE_DIST is reached
-    if (path.size() == 0) {
-        return;
-    }
-
-    Location dir;
-    double dist = 0.0;
-
-    if (path.size() == 1) {
-        const int currentX = std::floor(pose.getX() / MAP_RESOLUTION);
-        const int currentY = std::floor(pose.getY() / MAP_RESOLUTION);
-
-        dir.x = path.at(0).x - currentX;
-        dir.y = path.at(0).y - currentY;
-
-        dist = getPhysicalDist({currentX, currentY}, path.at(0));
-    } else {
-        dir = Location{path.at(1).x - path.at(0).x, path.at(1).y - path.at(0).y};
-        int stopIdx = -1;
-
-        for (int i = 0; i < path.size(); i++) {
-            if (i == 0) continue;
-
-            const int dirX = path.at(i).x - path.at(i - 1).x;
-            const int dirY = path.at(i).y - path.at(i - 1).y;
-
-            const double tempDist = getPhysicalDist(path.at(i), path.at(0));
-
-            if (
-                dirX != dir.x || dirY != dir.y ||
-                tempDist > MAX_MOVE_DIST
-            ) {
-                stopIdx = i - 1;
-                break;
-            }
-
-            if (i == path.size() - 1) {
-                stopIdx = i;
-                break;
-            }
-        }
-
-        dist = getPhysicalDist(path.at(stopIdx), path.at(0));
-    }
-
-    const double targetAngle = atan2(dir.y, dir.x) / PI * 180.0;
-    double angleDelta = targetAngle - fmod(pose.getTheta(), 360.0);
-
-    if (abs(angleDelta) > 180.0) {
-        if (angleDelta > 0) {
-            angleDelta -= 180.0;
-        } else {
-            angleDelta += 180.0;
-        }
-
-        angleDelta *= -1;
-    }
-
-    delay(100);
-
-    turnAngle(angleDelta, mL, mR, pose, eL, eR);
-
-    delay(100);
-
-    driveDistance(dist, mL, mR, pose, eL, eR);
-
-    delay(100);
+    // Convert back to degrees
+    pose.setTheta(thetaNew * 180.00 / PI);
 }
 
 void setup() {
-    // Ensure buttons are inputs
-    pinMode(SENSE_BUTTON, INPUT);
-    pinMode(PLAN_BUTTON, INPUT);
-    pinMode(ACT_BUTTON, INPUT);
-    pinMode(CONTINUE_BUTTON, INPUT);
+    // Buttons to move the robot around (forward, left, and right)
+    pinMode(1, INPUT);
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
 
-    pose.setX(START_X);
-    pose.setY(START_Y);
-
-    // First run through of the routine is directed by the user
-    while (!digitalRead(SENSE_BUTTON)) {}
-    while (digitalRead(SENSE_BUTTON)) {}
-
-    updateMap(pose, map, tof, mL, mR, eL, eR);
-
-    while (!digitalRead(PLAN_BUTTON)) {}
-    while (digitalRead(PLAN_BUTTON)) {}
-
-    const auto p = plan();
-
-    while (!digitalRead(ACT_BUTTON)) {}
-    while (digitalRead(ACT_BUTTON)) {}
-
-    act(p);
-
-    while (!digitalRead(CONTINUE_BUTTON)) {}
-    while (digitalRead(CONTINUE_BUTTON)) {}
-
-    // After that, run until the robot is at the target position
-    while (abs(pose.getX() - GOAL_X) > 0.2 || abs(pose.getY() - GOAL_Y) > 0.2) {
-        // Sense
-        updateMap(pose, map, tof, mL, mR, eL, eR);
-
-        // Plan
-        const auto path = plan();
-
-        // Act
-        act(path);
-    }
+    // Set initial position
+    pose.setX(0.5);
+    pose.setY(1.0);
 }
 
-void loop() {}
+void loop() {
+    // Call our function to update our position
+    updatePosition();
+
+    // Movement code
+    if (digitalRead(1)) {
+        left.run(255);
+        right.run(255);
+    } else if (digitalRead(2)) {
+        left.run(-255);
+        right.run(255);
+    } else if (digitalRead(3)) {
+        left.run(255);
+        right.run(-255);
+    } else {
+        left.run(0);
+        right.run(0);
+    }
+}
