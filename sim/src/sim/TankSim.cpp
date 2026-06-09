64cbf6921b91526b051e12c273a4fe3af55a3eef
West <westly.bouchard@gmail.com>
2026-06-09 13:07:24 -0600

    feat: Add telemetry section in TankSim that reports the current PWM setpoints of the motors.

--- sim/src/sim/TankSim.cpp
+++ sim/src/sim/TankSim.cpp
@@ -255,6 +255,18 @@ void TankSim::write() const {
         Telemetry::text(ss.str());
     }
 
+    if (Telemetry::section("Motors")) {
+        ss.str("");
+        ss << "Left Setpoint (PWM):  " << leftMotor->getPWM();
+        Telemetry::text(ss.str());
+
+        ss.str("");
+
+        ss << "Right Setpoint (PWM): " << rightMotor->getPWM();
+        Telemetry::text(ss.str());
+        ss.str("");
+    }
+
     if (config.showEncoderTelemetry) {
         ss.str("");
         if (Telemetry::section("Encoders")) {
